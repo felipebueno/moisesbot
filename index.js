d511aa5bb089862dc0fbeba63c27760ffb614d76
Felipe Bueno <bueno.felipe@gmail.com>
2018-10-06 22:25:20 -0300

    Add postgres stuff

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const PORT = process.env.PORT || 5000
 
 const SlackBot = require("slackbots");
 
+const {
+  Pool
+} = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+
 const reponses = [
   "É, eu vim... vim na caravana do SBT, mesmo, lá da Barra Funda.",
   "Eu sou do Cambuci.",
@@ -81,4 +90,19 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = {
+        'results': (result) ? result.rows : null
+      };
+      res.render('pages/db', results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
